06d2fad48fb3f3fa08e1c1af19a2f5b13b590e24
braydenvt <braydenvanteeling@outlook.com>
2025-01-26 15:20:38 -0700

    Add project files.

---
+++ Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPT_391_Project
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Load(object sender, EventArgs e)
+        {
+            btnLogin.Dock = DockStyle.Right | DockStyle.Left;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            try { 
+                //accessing db to sign in
+            }
+            catch (Exception){}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Register registrationWindow = new Register();
+            registrationWindow.Show();
+            this.Hide();
+        }
+    }
+}
