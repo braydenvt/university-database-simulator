da69ce5987a2a649df7136a82107315f3137442c
Daxton-C <164939824+Daxton-C@users.noreply.github.com>
2025-02-02 12:05:17 -0700

    Add files via upload

--- CMPT-391-Project/Register.cs
+++ CMPT-391-Project/Register.cs
@@ -16,5 +16,92 @@ namespace CMPT_391_Project
         {
             InitializeComponent();
         }
+
+        private void QueryButton_Click(object sender, EventArgs e)
+        {
+            // Shows courses in the database, based upon the entered fields
+            var DeptText = Dept.Text;
+            var IDText = ID.Text;
+            var SemText = Sem.Text;
+
+            if (DeptText.Length == 0 || IDText.Length == 0 || SemText.Length == 0)
+            {
+                
+            } else
+            {
+
+            }
+        }
+
+        private void VerifyButton_Click(object sender, EventArgs e)
+        {
+            // Checks if the student exists in the database and can therefore add the courses to their cart.
+            var SIDText = ID.Text;
+            var PassText = Pass.Text;
+
+            // Cannot access student if either field is blank.
+            if (SIDText.Length == 0 || PassText.Length == 0)
+            {
+                MessageBox.Show("ID or password is not entered.");
+            } else
+            {
+                // Perform queries...
+
+            }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CartDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check what column was clicked, to enroll in a course or delete a course from the cart
+            if (CartDataGrid.Columns[e.ColumnIndex].Name== "CartEnroll")
+            {
+                if (MessageBox.Show("Are you sure want to enroll in this course ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // Enrolls the course into the students' page
+                    return;
+                }
+            }
+
+            if (CartDataGrid.Columns[e.ColumnIndex].Name == "CartDelete")
+            {
+                if (MessageBox.Show("Are you sure want to remove this course from your cart ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // Removes the course from the cart
+                    return;
+                }
+            }
+        }
+
+        private void CourseSearchGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check what column was clicked to add a course to the cart.
+            if (CourseSearchGrid.Columns[e.ColumnIndex].Name == "Col_CartAdd")
+            {
+                if (MessageBox.Show("Are you sure want to add this course to your cart ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // Removes the course from the cart
+                    return;
+                }
+            }
+
+        }
+
+        private void EnrollDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check what column was clicked to unenroll from a course.
+            if (CourseSearchGrid.Columns[e.ColumnIndex].Name == "Col_CartAdd")
+            {
+                if (MessageBox.Show("Are you sure want to unenroll in this course ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    // Removes the course from the cart
+                    return;
+                }
+            }
+        }
     }
 }
