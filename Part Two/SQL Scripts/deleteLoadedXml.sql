02e2aac72307a66e35c21eb30727c79eb27145b4
dengf4 <dengf4@mymacewan.ca>
2025-03-06 13:46:49 -0700

    Update deleteLoadedXml.sql

--- Part Two/SQL Scripts/deleteLoadedXml.sql
+++ Part Two/SQL Scripts/deleteLoadedXml.sql
@@ -9,25 +9,25 @@ BEGIN
 	BEGIN Try
 		BEGIN Transaction
 
-            DELETE FROM dbo.Date
-            WHERE DateId >= 49;
-		
-            DELETE FROM dbo.Instructor 
-            WHERE InstructorId >= 1000;
-
-            DELETE FROM dbo.Course 
-            WHERE CourseId >= 10000;
-
-            DELETE FROM dbo.Student 
-            WHERE StudentId >= 200000;
-
-            -- This is probably not necessary because of the cascading delete constraints
-
-            -- DELETE FROM dbo.Enrollment
-            -- WHERE CourseId >= 10000
-            -- OR InstructorId >= 1000
-            -- OR StudentId >= 200000
-            -- OR DateId >= 49;
+	            DELETE FROM dbo.Date
+	            WHERE DateId >= 49;
+			
+	            DELETE FROM dbo.Instructor 
+	            WHERE InstructorId >= 1000;
+	
+	            DELETE FROM dbo.Course 
+	            WHERE CourseId >= 10000;
+	
+	            DELETE FROM dbo.Student 
+	            WHERE StudentId >= 200000;
+	
+	            -- This is probably not necessary because of the cascading delete constraints
+	
+	            -- DELETE FROM dbo.Enrollment
+	            -- WHERE CourseId >= 10000
+	            -- OR InstructorId >= 1000
+	            -- OR StudentId >= 200000
+	            -- OR DateId >= 49;
 
 		COMMIT Transaction
 	END Try
